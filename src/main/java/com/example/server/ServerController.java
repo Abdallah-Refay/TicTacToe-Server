c27699f8c1f7ff7f1ce49708ae52885d7603b09f
mohamedsambo <mohamedsambomahmoud@gmail.com>
2022-02-11 16:17:29 +0200

    third commit from @sambo creating creating validation functions for user name in back end if mathces regex and in backend if exist or not , creating hashing function for password by sha1 function

--- src/main/java/com/example/server/ServerController.java
+++ src/main/java/com/example/server/ServerController.java
@@ -3,6 +3,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import java.sql.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class ServerController  {
@@ -33,15 +38,55 @@ public class ServerController  {
     public String getSignUpPassword(){
         return signUpPassword .getText();
     }
-    public void validatLogIn(){
-        ResultSet rs = serverBuild.player_rs;
-        getUserName();
-        getPassword();
+
+
+    //ensure that input field of alphanumeric and have only _ char
+    public String validatUserName(String input){
+        String regex = "^[A-Za-z]\\w{5,29}$";
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+        // If the username is empty return false
+        //if (input == null) { return false; }
+        // Pattern class contains matcher() method to find matching between given username and regular expression.
+        Matcher m = p.matcher(input);
+        if(m.matches()) return input;
+        else return null;
     }
+    //hashing input field of password
+    public String hashingPassword(String input){
+        try {
+            // getInstance() method is called with algorithm SHA-1
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            // digest() method is called
+            // to calculate message digest of the input string
+            // returned as array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            // Add preceding 0s to make it 32 bit
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            // return the HashText
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //login through function in database query on UserName input and encrypted password
     public void playerLogIn() throws SQLException {
-        if(userName.getText().isBlank()==false &&Password.getText().isBlank()==false){
+        //using validUserName and encryptPass to insert successfully
+        String validUserName = validatUserName(getUserName());
+        String encryptPass = hashingPassword(getPassword());
+        if(getUserName().isBlank()==false &&getPassword().isBlank()==false){
             System.out.println( "logging in ..................." );
-            if( serverBuild.logIn(getUserName() , getPassword())){
+            if( serverBuild.logIn(validUserName , encryptPass)){
                 System.out.println("hello "+getUserName());
             }
             else{
@@ -54,11 +99,16 @@ public class ServerController  {
 
 
     }
+    //sign up through function in database query on UserName input and encrypted password
     public void playerSignUp() throws SQLException {
         if(signUpUserName.getText().isBlank()==false &&signUpPassword.getText().isBlank()==false){
+            //using validUserName and encryptPass to insert successfully
+            String validUserName = validatUserName(getSignUpUserName());
+            String encryptPass = hashingPassword(getSignUpPassword());
+            //------------------------------------------------------------------
             System.out.println( "Signing Up ..................." );
-            if( serverBuild.sigUp(getSignUpUserName() , getSignUpPassword())){
-                System.out.println("Welcome new member "+getSignUpUserName());
+            if( serverBuild.sigUp(validUserName , encryptPass)){
+                System.out.println("Welcome new member "+validUserName);
             }
             else{
                 System.out.println("Existing user name or password");
@@ -92,7 +142,6 @@ public class ServerController  {
         if(!serverBuild.conn.isClosed()){
             serverBuild.conn.close();
             System.out.println("Server Databases is down now");
-
         }
         else{
             System.out.println("Server is down already");
--- src/main/java/com/example/server/ServerDatabases.java
+++ src/main/java/com/example/server/ServerDatabases.java
@@ -47,28 +47,49 @@ public class ServerDatabases {
         }
     }
 
-    //using prepared statement
+
+    //using prepared statement to query on log in user name and encrypted password
     public boolean logIn(String username, String password) throws SQLException{
         PreparedStatement preSt = this.conn.prepareStatement("SELECT username, password FROM Players WHERE username= ? AND password= ?");
         preSt.setString(1,username);preSt.setString(2,password);
         ResultSet results = preSt.executeQuery();
         if(results.next()){
-            if(username.equals(results.getString(1)) && password.equals(results.getString(2)))
+            if(username.equals(results.getString(1)) && password.equals(results.getString(2))){
+                System.out.println(" yes exist user name and encrypted password ");
                 return true;
+            }
+
             else
                 return false;
         }
         else
             return false;
     }
-    //using prepared statement
-    public boolean sigUp(String username, String password) throws SQLException{
-        PreparedStatement preSt = this.conn.prepareStatement("INSERT INTO Players (username, password) VALUES (?,?)");
-        preSt.setString(1,username);preSt.setString(2,password);
-        int results = preSt.executeUpdate();
-        if(results ==1)
-            return true ;
-        else
+    //check if new user name exist or not used only before sign up
+    public boolean isExistUserName(String username) throws SQLException {
+        PreparedStatement preSt = this.conn.prepareStatement("SELECT username, password FROM Players WHERE username= ? ");
+        preSt.setString(1,username);
+        ResultSet results = preSt.executeQuery();
+        if(results.next()){
+            if(username.equals(results.getString(1)))
+                return true;
+            else
+                return false;
+        }
+        else return false ;
+    }
+    //using prepared statement to insert username and password
+    public boolean sigUp(String username, String password) throws SQLException {
+        if (!isExistUserName(username)) {
+            PreparedStatement preSt = this.conn.prepareStatement("INSERT INTO Players (username, password) VALUES (?,?)");
+            preSt.setString(1, username);
+            preSt.setString(2, password);
+            int results = preSt.executeUpdate();
+            if (results == 1)
+                return true;
+            else
+                return false;
+        } else
             return false;
     }
 
