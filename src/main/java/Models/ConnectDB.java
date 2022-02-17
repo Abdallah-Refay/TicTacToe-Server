6a5ace1115f2b85ec63e013a501d6d99c8a61c90
mohamedsambo <mohamedsambomahmoud@gmail.com>
2022-02-17 23:16:18 +0200

    adding connection database class

--- src/main/java/Models/ConnectDB.java
+++ src/main/java/Models/ConnectDB.java
@@ -1,4 +1,19 @@
 package Models;
 
-public class ConnectDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class ConnectDB {
+
+    public Connection getConnection() throws SQLException {
+        //database uploaded on AWS EC2 instance
+        return DriverManager.getConnection("jdbc:mysql://18.159.211.138:3306/xo_db", "sambo", "Sambo@123");
+    }
+
 }
+
+
+
+
+
