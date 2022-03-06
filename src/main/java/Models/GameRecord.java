ebf839e0397a812b1a305837e4fdb9682844c4b5
Abdallah Refay <abdallah.refay1999@gmail.com>
2022-03-06 17:04:25 +0200

    added findPausedGames Function to Game Record

--- src/main/java/Models/GameRecord.java
+++ src/main/java/Models/GameRecord.java
@@ -13,77 +13,18 @@ public class GameRecord {
     private int move;
     private int position;
 
-    public int getGameID() {
-        return gameID;
-    }
-    public void setGameID(int gameID) {
-        this.gameID = gameID;
-    }
-    public int getPlayerID() {
-        return playerID;
-    }
-    public void setPlayerID(int playerID) {
-        this.playerID = playerID;
-    }
-    public int getStepNumber() {
-        return stepNumber;
-    }
-    public void setStepNumber(int stepNumber) {
-        this.stepNumber = stepNumber;
-    }
-    public int getMove() {
-        return move;
-    }
-    public void setMove(int move) {
-        this.move = move;
-    }
-    public int getPosition() {
-        return position;
-    }
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    public GameRecord create(int gameID, int playerID, int move, int position) {
-        ConnectDB connectDB = new ConnectDB();
-        GameRecord gameRecord = new GameRecord();
-
-        String sql = "insert into moves (game_id, player_id, sign, pos) values (?, ?, ?, ?)";
-
-        try (Connection con = connectDB.getConnection(); PreparedStatement st = con.prepareStatement(sql);) {
-
-            //st.setInt(1, stepNumber);
-            st.setInt(1, gameID);
-            st.setInt(2, playerID);
-            st.setInt(3, move);
-            st.setInt(4, position);
-
-            st.executeUpdate();
-            gameRecord.setGameID(gameID);
-            gameRecord.setStepNumber(stepNumber);
-            gameRecord.setMove(move);
-            gameRecord.setPlayerID(playerID);
-            gameRecord.setPosition(position);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            gameRecord = null;
-        }
-
-        return gameRecord;
-    }
-    public static  ArrayList<PausedGames> findPausedGames(int plyer1Id) {
+    public static ArrayList<PausedGames> findPausedGames(int plyer1Id) {
         ConnectDB connectDB = new ConnectDB();
         ArrayList<PausedGames> pausedGames = new ArrayList<>();
         PausedGames game;
-        Player player ;
+        Player player;
         //String sql1 = "SELECT game_id  FROM  game WHERE IsFinished = false ";
         String sql1 = "SELECT  g.game_id" +
                 " FROM moves as m , game as g" +
                 " where  m.game_id = g.game_id and g.IsFinished = false" +
                 " Group by g.game_id ,m.player_id" +
-                " having m.player_id =?" ;
-        try (Connection con = connectDB.getConnection(); PreparedStatement st = con.prepareStatement(sql1); ) {
+                " having m.player_id =?";
+        try (Connection con = connectDB.getConnection(); PreparedStatement st = con.prepareStatement(sql1)) {
             st.setInt(1, plyer1Id);
             ResultSet rs = st.executeQuery();
             while (rs.next()) {
@@ -107,7 +48,7 @@ public class GameRecord {
                 st.setInt(2, plyer1Id);
                 ResultSet rs = st.executeQuery();
                 while (rs.next()) {
-                    player = new Player() ;
+                    player = new Player();
                     pausedGame.setOpponentId(rs.getInt(1));
                     pausedGame.setOpponent(player.findID(rs.getInt(1)).getUsername());
                 }
@@ -117,6 +58,75 @@ public class GameRecord {
             }
         }
         return pausedGames;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public int getStepNumber() {
+        return stepNumber;
+    }
+
+    public void setStepNumber(int stepNumber) {
+        this.stepNumber = stepNumber;
+    }
+
+    public int getMove() {
+        return move;
+    }
+
+    public void setMove(int move) {
+        this.move = move;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public GameRecord create(int gameID, int playerID, int move, int position) {
+        ConnectDB connectDB = new ConnectDB();
+        GameRecord gameRecord = new GameRecord();
+
+        String sql = "insert into moves (game_id, player_id, sign, pos) values (?, ?, ?, ?)";
+
+        try (Connection con = connectDB.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
+
+            //st.setInt(1, stepNumber);
+            st.setInt(1, gameID);
+            st.setInt(2, playerID);
+            st.setInt(3, move);
+            st.setInt(4, position);
+
+            st.executeUpdate();
+            gameRecord.setGameID(gameID);
+            gameRecord.setStepNumber(stepNumber);
+            gameRecord.setMove(move);
+            gameRecord.setPlayerID(playerID);
+            gameRecord.setPosition(position);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            gameRecord = null;
+        }
+
+        return gameRecord;
     }
 
     public ArrayList<GameRecord> findByGameID(int id) {
