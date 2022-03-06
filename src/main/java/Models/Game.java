811fb7b0a0f320a148867740b9fdc928d9cd2aeb
Abdallah Refay <abdallah.refay1999@gmail.com>
2022-03-06 17:03:08 +0200

    added Pause and create game required function to game Model

--- src/main/java/Models/Game.java
+++ src/main/java/Models/Game.java
@@ -5,18 +5,24 @@ import java.sql.*;
 public class Game {
     private int id;//game id
     private String winner; // user name of winner
+    private boolean IsFinished;
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getWinner() {
         return winner;
     }
+
     public void setWinner(String winner) {
         this.winner = winner;
     }
+
     public Game create() {
         ConnectDB connectDB = new ConnectDB();
         Game game = new Game();
@@ -30,6 +36,7 @@ public class Game {
         }
         return game;
     }
+
     public Game findByID(int id) {
         ConnectDB connectDB = new ConnectDB();
         String sql = "select * from game where game_id = ?";
@@ -47,11 +54,12 @@ public class Game {
         }
         return game;
     }
+
     public int getLatestGameID() {
         ConnectDB connectDB = new ConnectDB();
         String sql = "select count(game_id) from game";
         int latestGameID = 0;
-        try (Connection con = connectDB.getConnection(); Statement st = con.createStatement(); ResultSet rs = st.executeQuery(sql);) {
+        try (Connection con = connectDB.getConnection(); Statement st = con.createStatement(); ResultSet rs = st.executeQuery(sql)) {
             while (rs.next()) {
                 latestGameID = rs.getInt(1);
             }
@@ -60,12 +68,14 @@ public class Game {
         }
         return latestGameID;
     }
+
     public void finishGame(int id, String winnerUsername) {
         Game game = new Game();
         game = game.findByID(id);
         game.setWinner(winnerUsername);
         game.save();
     }
+
     public void save() {
         ConnectDB connectDB = new ConnectDB();
         String sql = "update game set winner = ? where game_id = ?";
@@ -77,4 +87,60 @@ public class Game {
             e.printStackTrace();
         }
     }
+
+    public Game create(int id) {
+        ConnectDB connectDB = new ConnectDB();
+        Game game = new Game();
+        String sql = "insert into game (game_id ) values (?) ";
+        try (Connection con = connectDB.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
+            st.setInt(1, id);
+            st.execute();
+            game.setId(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return game;
+    }
+
+    public void pauseGame(int id) {
+        Game game = new Game();
+        game = game.findByID(id);
+        game.setGameState(false);
+        game.savePaused();
+    }
+
+    public void setGameState(boolean test) {
+        IsFinished = test;
+    }
+
+    public boolean getGameState(int gameId) {
+        ConnectDB connectDB = new ConnectDB();
+        String sql = "select * from game where game_id = ? and IsFinished= false";
+        Game game = null;
+        try (Connection con = connectDB.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
+            st.setInt(1, gameId);
+            ResultSet rs = st.executeQuery();
+            game = new Game();
+            while (rs.next()) {
+                game.setId(rs.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return game != null;
+    }
+
+    public void savePaused() {
+        ConnectDB connectDB = new ConnectDB();
+        String sql2 = "update game set IsFinished = false  where game_id = ?";
+        try (Connection con = connectDB.getConnection(); PreparedStatement st = con.prepareStatement(sql2)) {
+            //st.setString(1, this.winner);
+            st.setInt(1, this.id);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
